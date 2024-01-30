7fdcdaf8f755cafa167c1d81503d3beadce5a16d
Mahdi_Shavvalpour <m.shavval@gmail.com>
2024-01-30 18:17:21 +0330

    add full validation for person when is creaing

--- TaskManagment.Api/Features/People/CreatePerson/CreatePerson.Validator.cs
+++ TaskManagment.Api/Features/People/CreatePerson/CreatePerson.Validator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace TaskManagement.Api.Features.People.CreatePerson;
 
@@ -8,7 +9,27 @@ public class Validator : AbstractValidator<Command>
     {
         RuleFor(x => x.FirstName)
             .MaximumLength(50)
-            .WithMessage("");
+            .WithMessage("first name must not exceed 50 characters.");
+
+        RuleFor(x => x.LastName)
+           .MaximumLength(50)
+           .WithMessage("last name must not exceed 50 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email Is Required")
+            .EmailAddress()
+            .WithMessage("")
+            .MaximumLength(100)
+            .WithMessage("email must not exceed 100 characters . ");
+
+        RuleFor(p => p.PhoneNumber)
+       .MinimumLength(11)
+       .WithMessage("PhoneNumber must not be less than 11 characters.")
+       .MaximumLength(11)
+       .WithMessage("PhoneNumber must not exceed 11 characters.")
+       .Matches(new Regex("^(\\+98|0)?9\\d{9}$"))
+       .WithMessage("PhoneNumber not valid");
 
     }
 }
