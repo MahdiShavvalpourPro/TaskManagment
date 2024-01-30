3732b40e5ab81a6a35e675fd333f24a923c36bbd
Mahdi_Shavvalpour <m.shavval@gmail.com>
2024-01-30 18:50:28 +0330

    modify person validation when is creaging

--- TaskManagment.Api/Features/People/CreatePerson/CreatePerson.Validator.cs
+++ TaskManagment.Api/Features/People/CreatePerson/CreatePerson.Validator.cs
@@ -1,21 +1,29 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
+using TaskManagement.Infrastructure;
 
 namespace TaskManagement.Api.Features.People.CreatePerson;
 
 public class Validator : AbstractValidator<Command>
 {
-    public Validator()
+    private readonly TaskManagementDbContext _context;
+
+    public Validator(TaskManagementDbContext context)
     {
+        _context = context;
+
         RuleFor(x => x.FirstName)
-            .MaximumLength(50)
-            .WithMessage("first name must not exceed 50 characters.");
+           .MaximumLength(50)
+           .WithMessage("first name must not exceed 50 characters.");
 
         RuleFor(x => x.LastName)
            .MaximumLength(50)
            .WithMessage("last name must not exceed 50 characters.");
 
         RuleFor(x => x.Email)
+            .MustAsync(BeUniqueEmail)
+            .WithMessage("this email already exists.")
             .NotEmpty()
             .WithMessage("Email Is Required")
             .EmailAddress()
@@ -24,12 +32,28 @@ public class Validator : AbstractValidator<Command>
             .WithMessage("email must not exceed 100 characters . ");
 
         RuleFor(p => p.PhoneNumber)
+       .MustAsync(BeUniqueMobileNumber)
+       .WithMessage("this phone number already exists.")
        .MinimumLength(11)
        .WithMessage("PhoneNumber must not be less than 11 characters.")
        .MaximumLength(11)
        .WithMessage("PhoneNumber must not exceed 11 characters.")
        .Matches(new Regex("^(\\+98|0)?9\\d{9}$"))
        .WithMessage("PhoneNumber not valid");
+    }
+
+    private Task<bool> BeUniqueMobileNumber(string phoneNumber,
+        CancellationToken cancellationToken)
+    {
+        return _context.Tbl_Persons
+            .AllAsync(x => x.PhoneNumber != phoneNumber, cancellationToken);
+    }
+
+    private Task<bool> BeUniqueEmail(string email,
+        CancellationToken cancellationToken)
+    {
+        return _context.Tbl_Persons
+            .AllAsync(x => x.Email != email, cancellationToken);
 
     }
 }
