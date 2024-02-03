3c6f5fa94cf8e69a4cbb1090df10b57e32e58a07
Mahdi_Shavvalpour <m.shavval@gmail.com>
2024-02-03 12:18:59 +0330

    add contruc for base entity

--- TaskManagment.Application/Entities/BaseEntity.cs
+++ TaskManagment.Application/Entities/BaseEntity.cs
@@ -2,6 +2,11 @@
 {
     public class BaseEntity
     {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+            IsRemoved = false;
+        }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public bool IsRemoved { get; set; }
